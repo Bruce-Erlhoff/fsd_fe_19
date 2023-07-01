169738763e6c541bf6f02b158fc5b8131a3f8a65
Bruce-Erlhoff <bruce.erlhoff@gmail.com>
2023-06-30 23:08:16 -0700

    first commit

---
+++ src/hello.js
@@ -0,0 +1,14 @@
+const Aloha = () => {
+    return (
+    <fragment>
+        <br></br>
+        <h1>Aloha World!</h1>
+        <img src="aloha.jpg" alt="aloha" width="100%"></img>
+        <br></br>
+        <br></br>
+
+    </fragment>
+    );
+}
+ 
+export default Aloha;
